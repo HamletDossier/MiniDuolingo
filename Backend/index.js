0fd3f01f035d9dd9b15035669255482894f1b58c
Hamlet <dossier@hamletideas.com>
2022-10-26 18:33:16 -0500

    Connection Atlas Mongodb

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,11 @@
+import "dotenv/config";
+import "./database/connectdb.js";
 import express from "express";
-const app = express();
+const app = express(); 
 
+//*This line is say Ok
 app.get('/',(req,res)=>{res.json({'ok':true})})
-app.listen(5000,()=>{
-	console.log('http://localhost:5000');
-});
+//*This line is Config PORT 
+const PORT = process.env.PORT || 5000;
+//* Listen the PORT
+app.listen(PORT,()=>console.log('http://localhost:'+ PORT));
